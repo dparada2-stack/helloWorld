e85ed3f7623fba2474e1b4871c69499639fd0357
Daniel Parada <dparada2@terpmail.umd.edu>
2026-09-24 21:23:55 -0400

    Add /about route

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def hello_world():
 def hello():
     return render_template('hello.html')
 
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
